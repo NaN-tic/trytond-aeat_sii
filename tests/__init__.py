856a62e4bed7924983fe8665d24009d92f57ebe0
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 13:58:16 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.aeat_sii.tests.test_aeat_sii import suite
-except ImportError:
-    from .test_aeat_sii import suite
-
-__all__ = ['suite']
---
+++ tests/test_module.py
@@ -0,0 +1,24 @@
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.modules.aeat_sii.tools import unaccent
+from trytond.modules.company.tests import CompanyTestMixin
+
+
+class AeatSiiTestCase(CompanyTestMixin, ModuleTestCase):
+    'Test AeatSii module'
+    module = 'aeat_sii'
+
+    def test_unaccent(self):
+        for value, result in [
+                ('aeiou', b'aeiou'),
+                ('áéíóú', b'aeiou'),
+                ('__aéiou__', b'aeiou'),
+                ('__aé@ou__', b'aeou'),
+                ]:
+            self.assertEqual(unaccent(value), result)
+
+
+del ModuleTestCase
