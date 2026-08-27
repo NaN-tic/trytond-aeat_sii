913efa3fd66b1aa9bc29dfb895e101e1b8625e63
Raimon Esteve <raimonesteve@gmail.com>
2026-08-27 11:07:52 +0200

    FIX Add mock patch VIES valid VAT Check the validity of the European tax identifiers when posting invoice issue14931

--- tests/test_scenario_aeat_sii.py
+++ tests/test_scenario_aeat_sii.py
@@ -1,8 +1,10 @@
 import datetime
 import unittest
 from decimal import Decimal
+from unittest.mock import patch
 
 from dateutil.relativedelta import relativedelta
+from stdnum import get_cc_module
 from proteus import Model, Wizard
 from trytond.exceptions import UserWarning
 from trytond.modules.account_es.tests.tools import (create_chart, create_tax,
@@ -19,10 +21,19 @@ from trytond.tests.tools import activate_modules
 class Test(unittest.TestCase):
 
     def setUp(self):
+        self.vies_patcher = patch.object(
+            get_cc_module('eu', 'vat'), 'check_vies',
+            return_value={
+                'valid': True,
+                'name': 'Test Party',
+                'address': 'Test Address',
+                })
+        self.vies_patcher.start()
         drop_db()
         super().setUp()
 
     def tearDown(self):
+        self.vies_patcher.stop()
         drop_db()
         super().tearDown()
 
--- tests/test_scenario_invoice_in_without_ref.py
+++ tests/test_scenario_invoice_in_without_ref.py
@@ -1,7 +1,9 @@
 import datetime
 import unittest
 from decimal import Decimal
+from unittest.mock import patch
 
+from stdnum import get_cc_module
 from proteus import Model
 from trytond.exceptions import UserWarning
 from trytond.modules.account_es.tests.tools import (create_chart,
@@ -17,10 +19,19 @@ from trytond.tests.tools import activate_modules
 class Test(unittest.TestCase):
 
     def setUp(self):
+        self.vies_patcher = patch.object(
+            get_cc_module('eu', 'vat'), 'check_vies',
+            return_value={
+                'valid': True,
+                'name': 'Test Party',
+                'address': 'Test Address',
+                })
+        self.vies_patcher.start()
         drop_db()
         super().setUp()
 
     def tearDown(self):
+        self.vies_patcher.stop()
         drop_db()
         super().tearDown()
 
